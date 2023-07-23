ca303db0c1290864089c1126bb634b731e281e00
NinaGon <1472580369tygv@gmail.com>
2023-07-23 16:04:52 +0800

    add led app

--- inc/my_led_test.h
+++ inc/my_led_test.h
@@ -7,17 +7,24 @@
  * Date           Author           Notes
  * 2023-07-22     Ninagon666       the first version
  */
- 
-#ifndef __MY_LED_TEST_H__
-#define __MY_LED_TEST_H__
+
+#ifndef __PKG_MY_LED_TEST_H__
+#define __PKG_MY_LED_TEST_H__
+
+#include <rtthread.h>
+
 
 #ifndef PKG_USING_LED_PIN_NUM
-#define LED_PIN_NUM        (666)
+#define LED_PIN_NUM        (39)
 #else
 #define LED_PIN_NUM        PKG_USING_LED_PIN_NUM
 #endif
 
-#include <rtthread.h>
+#ifndef PKG_USING_TOGGLE_TICKS
+#define DEFAULT_TOGGLE_TICKS        (500)
+#else
+#define DEFAULT_TOGGLE_TICKS        PKG_USING_TOGGLE_TICKS
+#endif
 
 #ifdef __cplusplus
 extern "C"
--- src/my_led_test.c
+++ src/my_led_test.c
@@ -9,10 +9,44 @@
  */
  
 #include "my_led_test.h"
+#include <rtdevice.h>
+#include <board.h>
 
 static void show_my_led_test_version(void)
 {
-    rt_kprintf("v1.0.0 %d\n", LED_PIN_NUM);
+    rt_kprintf("my led test vesion is v1.0.0\n");
+    rt_kprintf("using the led pin num is %d\n", LED_PIN_NUM);
+    rt_kprintf("toggle led ticks is %d\n", DEFAULT_TOGGLE_TICKS);
 }
 FINSH_FUNCTION_EXPORT(show_my_led_test_version, show version on the terminal screen);
 MSH_CMD_EXPORT(show_my_led_test_version, show version on the terminal screen);
+
+static void led_app_entry(void *parameter)
+{
+    rt_pin_mode(LED_PIN_NUM, PIN_MODE_OUTPUT);
+  
+    while(1)
+    {
+        rt_pin_write(LED_PIN_NUM, PIN_HIGH);
+        rt_thread_mdelay(DEFAULT_TOGGLE_TICKS);
+        rt_pin_write(LED_PIN_NUM, PIN_LOW);
+        rt_thread_mdelay(DEFAULT_TOGGLE_TICKS);
+    }
+}
+
+static int my_led_app_init(void)
+{
+    rt_thread_t tid;
+  
+    //创建led线程 优先级设置为20
+    tid = rt_thread_create("led_app", led_app_entry, RT_NULL, 512, 20, 5);
+
+    //启动led线程
+    if (RT_NULL != tid)
+        rt_thread_startup(tid);  
+    
+     return 0;
+}
+#ifdef PKG_USING_MY_LED_TEST
+INIT_APP_EXPORT(my_led_app_init);
+#endif
